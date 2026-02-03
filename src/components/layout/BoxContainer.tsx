b05df7d03754f81b6e746f234302e8b5b979cd91
sadidft <mail.rafifsadid@gmail.com>
2026-02-04 02:37:19 +0700

    Update BoxContainer.tsx

--- src/components/layout/BoxContainer.tsx
+++ src/components/layout/BoxContainer.tsx
@@ -178,7 +178,7 @@ export function BoxContainer({ box }: BoxContainerProps) {
     <div className={`flex flex-col flex-1 min-h-0 ${box.isMaximized ? 'h-full' : ''}`}>
       <TabBar boxId={box.id} showWindowControls={true} onMinimize={handleMinimize} onMaximize={handleMaximize} onClose={handleClose} />
       <AddressBar />
-      <main className="flex-1 flex flex-col overflow-hidden">
+      <main className="flex-1 flex flex-col">
         <CardGrid />
       </main>
     </div>
